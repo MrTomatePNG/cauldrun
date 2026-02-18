9dce46ac6c9f85633fb8b6a66ee96225fcac6156
MrTomatePNG <mrtomate360@outlook.com>
2026-02-18 09:36:10 -0400

    ajuste de alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+      $lib: path.resolve("./src/lib"),
+      $prisma: path.resolve("./generated/prisma"),
+    },
+  },
 });

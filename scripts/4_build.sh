a8123c91295df1ee7e27947295e39afc3093ac7d
MrTomatePNG <mrtomate360@outlook.com>
2026-02-19 21:31:24 -0400

    Update 4_build.sh

--- scripts/4_build.sh
+++ scripts/4_build.sh
@@ -1,6 +1,11 @@
 # sem set -e
+set -e
 export PATH="/home/cauldrun/.bun/bin:$PATH"
 cd /opt/cauldrun
 echo "### 4/5: Buildando a aplicação... ###"
-NODE_OPTIONS="--trace-uncaught --trace-warnings" bun --bun vite build 2>&1
-echo "Exit code do build: $?"
+
+echo "=== VARS DISPONÍVEIS NO BUILD ==="
+printenv | sort
+echo "================================="
+
+bun --bun run build

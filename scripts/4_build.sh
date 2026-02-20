0eecdc3b504c37e3308206b517eea4b3826c035d
MrTomatePNG <mrtomate360@outlook.com>
2026-02-19 21:27:46 -0400

    Update 4_build.sh

--- scripts/4_build.sh
+++ scripts/4_build.sh
@@ -2,5 +2,5 @@
 export PATH="/home/cauldrun/.bun/bin:$PATH"
 cd /opt/cauldrun
 echo "### 4/5: Buildando a aplicação... ###"
-NODE_OPTIONS="--trace-uncaught --trace-warnings" bun --bun vite build --logLevel silly 2>&1
+NODE_OPTIONS="--trace-uncaught --trace-warnings" bun --bun vite build 2>&1
 echo "Exit code do build: $?"

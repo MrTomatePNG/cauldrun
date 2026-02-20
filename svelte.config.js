87c7c095092761c8a1b5490257a065d20048d6d9
MrTomatePNG <mrtomate360@outlook.com>
2026-02-19 21:24:32 -0400

    silence selector warnings and activate precompress

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,16 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      precompress: true,
+    }),
     alias: {
       "@/*": path.resolve("src/*"),
     },
+  },
+  onwarn: (warning, defaultHandler) => {
+    if (warning.code === "css_unused_selector") return;
+    defaultHandler(warning);
   },
   preprocess: sveltePreprocess({
     scss: {},
